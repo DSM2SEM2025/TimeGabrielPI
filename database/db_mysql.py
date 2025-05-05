04244cce551e51b173dafd7b196b8f1852eb36b1
d-Jhon-b <wwluiza.09@gmail.com>
2025-05-05 13:33:00 -0300

    atl

--- database/db_mysql.py
+++ database/db_mysql.py
@@ -1,4 +1,31 @@
 import os
 from dotenv import load_dotenv
+from mysql.connector import connect, Error
 
 load_dotenv()
+
+hostDB = 'localhost'
+usernameDB = 'c##atividadeDB'
+passwordDB = 'c##atividadeDB'
+portDB = 3306
+databaseDB = 'recriandoProcedures'
+
+def mysql_conn():
+    try:
+        conn = connect(
+            host = hostDB,
+            username=usernameDB,
+            password = passwordDB,
+            port = portDB,
+            database = databaseDB
+        )
+
+        return True, f'Sucesso ao conectar no banco', conn
+    except Error as err:
+        return False, f'Erro ao conectar no banco: {err}', conn
+    
+validation, msg, conn = mysql_conn()
+if conn:
+    print(msg)
+else:
+    print(msg)
